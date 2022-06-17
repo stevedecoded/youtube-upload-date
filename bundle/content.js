148432ff0ad03469b47f923f86dc47a84b01bafd
Steve McIntyre <stevedecoded@gmail.com>
2022-06-17 19:12:51 +0100

    v2.5 - hide button text and remove their margins/padding to fit buttons on same line as view count and upload date

--- bundle/content.js
+++ bundle/content.js
@@ -1,4 +1,31 @@
 let style = document.createElement("style")
 style.setAttribute("type", "text/css")
-style.innerHTML = "#info.ytd-video-primary-info-renderer { display:block !important; }"
+style.innerHTML =
+"#top-level-buttons-computed > *, \
+#top-level-buttons-computed a \
+{ padding: 0 !important; margin: 0 !important; } \
+"
 document.getElementsByTagName("head")[0].appendChild(style)
+
+setInterval(() => {
+    let outer = document.getElementById('menu-container')
+    if (!outer) {
+        return false
+    }
+
+    let container = outer.querySelector('.top-level-buttons')
+    if (!container || !container.childNodes.length) {
+        return false
+    }
+
+    let nodes = container.childNodes
+
+    for (let i = 0; i < nodes.length; i ++) {
+        if (parseInt(i) != 0) {
+            let text = container.childNodes[i].querySelector('yt-formatted-string')
+            if (text) {
+                text.innerText = ''
+            }
+        }
+    }
+}, 2000)
