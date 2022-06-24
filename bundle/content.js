1a3763862791c68c9b4df1b5ec5fe3d42942035a
Steve McIntyre <stevedecoded@gmail.com>
2022-06-24 01:31:27 +0100

    v2.6 - Return YouTube Dislike extension compatibility, ellipsis button padding removed

--- bundle/content.js
+++ bundle/content.js
@@ -2,30 +2,78 @@ let style = document.createElement("style")
 style.setAttribute("type", "text/css")
 style.innerHTML =
 "#top-level-buttons-computed > *, \
-#top-level-buttons-computed a \
+#top-level-buttons-computed a, \
+ytd-menu-renderer.ytd-video-primary-info-renderer > yt-icon-button \
 { padding: 0 !important; margin: 0 !important; } \
 "
 document.getElementsByTagName("head")[0].appendChild(style)
 
-setInterval(() => {
+function getButtonNodes() {
     let outer = document.getElementById('menu-container')
     if (!outer) {
-        return false
+        return null
     }
 
     let container = outer.querySelector('.top-level-buttons')
     if (!container || !container.childNodes.length) {
+        return null
+    }
+
+    return container.childNodes
+}
+
+function stopDislikePing() {
+    clearInterval(dislikePing)
+    dislikePing = null
+}
+
+function setDisplayStyle(el, state) {
+    if (el.style.display != state) {
+        el.style.display = state
+    }
+}
+
+let mainPing = setInterval(() => {
+    let nodes = getButtonNodes()
+
+    if (!nodes) {
         return false
     }
 
-    let nodes = container.childNodes
-
-    for (let i = 0; i < nodes.length; i ++) {
-        if (parseInt(i) != 0) {
-            let text = container.childNodes[i].querySelector('yt-formatted-string')
-            if (text) {
-                text.innerText = ''
-            }
+    for (let i = 2; i < nodes.length; i ++) {
+        let text = nodes[i].querySelector('yt-formatted-string')
+        if (text) {
+            setDisplayStyle(text, 'none')
         }
     }
-}, 2000)
+}, 200)
+
+let dislikePing = setInterval(() => {
+    let nodes = getButtonNodes()
+
+    if (!nodes) {
+        return false
+    }
+
+    let text = nodes[1].querySelector('yt-formatted-string')
+    if (text) {
+        if (isNaN(parseInt(text.innerText))) {
+            setDisplayStyle(text, 'none')
+        } else {
+            text.style.display = 'block'
+            let tooltip = document.querySelector('.ryd-tooltip')
+            if (tooltip) {
+                tooltip.style.width = (nodes[0].clientWidth
+                    + nodes[1].clientWidth
+                    + 8) + 'px'
+            }
+            stopDislikePing()
+        }
+    }
+}, 200)
+
+setTimeout(() => {
+    clearInterval(mainPing)
+    mainPing = null
+    stopDislikePing()
+}, 30000)
